ad0ede46d3426a419eee95051c2a8c3eaf9efcbf
Linh Tran <112348820+linh-tran-thao@users.noreply.github.com>
2025-06-09 23:28:00 -0400

    Create select & from.sql

---
+++ LessonS/select & from.sql
@@ -0,0 +1,15 @@
+-- Retrieve All Customer Data
+SELECT *
+FROM customers
+
+-- Retrieve All Order Data
+SELECT *
+FROM orders
+
+-- Retrieve each customer's name, country and score.
+
+SELECT
+  first_name,
+  country,
+  score
+FROM customers

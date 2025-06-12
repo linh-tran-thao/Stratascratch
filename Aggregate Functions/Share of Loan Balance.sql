798f11d23b9b066a82a2d9f2e6089fa6ba8772f0
Linh Tran <112348820+linh-tran-thao@users.noreply.github.com>
2025-06-11 22:30:51 -0400

    Update Share of Loan Balance.sql

--- Aggregate Functions/Share of Loan Balance.sql
+++ Aggregate Functions/Share of Loan Balance.sql
@@ -1,4 +1,5 @@
 -- Write a query that returns the rate_type, loan_id, loan balance , and a column that shows with what percentage the loan's balance contributes to the total balance among the loans of the same rate type. Sort the final output by rate_type and loan_id.
+-- Computes the total balance for all loans within the same rate_type group. This is a window function.
 
 select rate_type, loan_id, balance, (balance / SUM(balance) over (partition by rate_type)) * 100 as balance_share
 from submissions
